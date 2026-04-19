b02ba9c2c7824a89a2d1074781722d5dca0bc680
JorgeV92 <jorgevmg31@gmail.com>
2026-04-18 22:11:20 -0500

    stirngs and graphs in go

--- algorithms/graph/problems.go
+++ algorithms/graph/problems.go
@@ -2,17 +2,23 @@ package graph
 
 import (
 	"container/list"
+	"math/bits"
+	"slices"
 	"sort"
 )
 
 type Problem string
 
 const (
-	SurroundingXO   Problem = "surrounding-xo"
-	Topological     Problem = "topological-sort"
-	LineChart       Problem = "line-chart"
-	ObstacleRemoval Problem = "obstacle-removal"
-	IncreasingPaths Problem = "count-paths"
+	SurroundingXO         Problem = "surrounding-xo"
+	Topological           Problem = "topological-sort"
+	LineChart             Problem = "line-chart"
+	ObstacleRemoval       Problem = "obstacle-removal"
+	IncreasingPaths       Problem = "count-paths"
+	ParallelCourse2       Problem = "parallel-course"
+	MinimizeMalwareSpread Problem = "min-mal-spread"
+	ReachDestinantion     Problem = "reach-destination"
+	NumberOfGoodPaths     Problem = "num-of-good-paths"
 )
 
 func ProblemNames() []string {
@@ -222,4 +228,191 @@ func CountPaths(grid [][]int) (ans int) {
 		}
 	}
 	return
+}
+
+func MinNumOfSemesters(n int, realtions [][]int, k int) int {
+	d := make([]int, n+1)
+	for _, e := range realtions {
+		d[e[1]] |= 1 << e[0]
+	}
+	type pair struct{ v, t int }
+	q := []pair{{0, 0}}
+	vis := map[int]bool{0: true}
+	for len(q) > 0 {
+		p := q[0]
+		q = q[1:]
+		cur, t := p.v, p.t
+		if cur == (1<<(n+1) - 2) {
+			return t
+		}
+		nxt := 0
+		for i := 1; i <= n; i++ {
+			if (cur & d[i]) == d[i] {
+				nxt |= 1 << i
+			}
+		}
+		nxt ^= cur
+		if bits.OnesCount(uint(nxt)) <= k {
+			if !vis[nxt|cur] {
+				vis[nxt|cur] = true
+				q = append(q, pair{nxt | cur, t + 1})
+			}
+		} else {
+			x := nxt
+			for nxt > 0 {
+				if bits.OnesCount(uint(nxt)) == k && !vis[nxt|cur] {
+					vis[nxt|cur] = true
+					q = append(q, pair{nxt | cur, t + 1})
+				}
+				nxt = (nxt - 1) & x
+			}
+		}
+	}
+	return 0
+}
+
+type dsu struct {
+	p, size []int
+}
+
+func newDSU(n int) *dsu {
+	p := make([]int, n)
+	size := make([]int, n)
+	for i := range p {
+		p[i] = i
+		size[i] = 1
+	}
+	return &dsu{p, size}
+}
+
+func (d *dsu) find(x int) int {
+	if d.p[x] != x {
+		d.p[x] = d.find(d.p[x])
+	}
+	return d.p[x]
+}
+
+func (d *dsu) unite(x, y int) bool {
+	x, y = d.find(x), d.find(y)
+	if x == y {
+		return false
+	}
+	if d.size[x] < d.size[y] {
+		x, y = y, x
+	}
+	d.p[y] = x
+	d.size[x] += d.size[y]
+	return true
+}
+
+func (d *dsu) sz(x int) int {
+	return d.size[x]
+}
+
+func MinMalwareSpread(graph [][]int, initial []int) int {
+	n := len(graph)
+	d := newDSU(n)
+	for i := range graph {
+		for j := 0; j < n; j++ {
+			if graph[i][j] == 1 {
+				d.unite(i, j)
+			}
+		}
+	}
+	cnt := make([]int, n)
+	for _, x := range initial {
+		cnt[d.find(x)]++
+	}
+	ans, mx := n, 0
+	for _, x := range initial {
+		root := d.find(x)
+		if cnt[root] == 1 {
+			z := d.sz(root)
+			if z > mx || z == mx && ans > x {
+				ans = x
+				mx = z
+			}
+		}
+	}
+	if ans == n {
+		return slices.Min(initial)
+	}
+	return ans
+}
+
+func MinCost(maxTime int, edges [][]int, passingFees []int) int {
+	m, n := maxTime, len(passingFees)
+	inf := 1 << 30
+	f := make([][]int, m+1)
+	for i := range f {
+		f[i] = make([]int, n)
+		for j := range f[i] {
+			f[i][j] = inf
+		}
+	}
+	f[0][0] = passingFees[0]
+	for i := 1; i <= m; i++ {
+		for _, e := range edges {
+			x, y, t := e[0], e[1], e[2]
+			if t <= i {
+				f[i][x] = min(f[i][x], f[i-t][y]+passingFees[x])
+				f[i][y] = min(f[i][y], f[i-t][x]+passingFees[y])
+			}
+		}
+	}
+	ans := inf
+	for i := 1; i <= m; i++ {
+		ans = min(ans, f[i][n-1])
+	}
+	if ans == inf {
+		return -1
+	}
+	return ans
+}
+
+func NumOfGoodPaths(vals []int, edges [][]int) int {
+	n := len(vals)
+	pr := make([]int, n)
+	size := map[int]map[int]int{}
+	type pair struct{ v, i int }
+	arr := make([]pair, n)
+	var find func(x int) int
+	find = func(x int) int {
+		if x != pr[x] {
+			pr[x] = find(pr[x])
+		}
+		return pr[x]
+	}
+	for i, v := range vals {
+		pr[i] = i
+		if size[i] == nil {
+			size[i] = map[int]int{}
+		}
+		size[i][v] = 1
+		arr[i] = pair{v, i}
+	}
+	sort.Slice(arr, func(i, j int) bool { return arr[i].v < arr[j].v })
+	g := make([][]int, n)
+	for _, e := range edges {
+		a, b := e[0], e[1]
+		g[a] = append(g[a], b)
+		g[b] = append(g[b], a)
+	}
+	ans := n
+	for _, e := range arr {
+		v, x := e.v, e.i
+		for _, y := range g[x] {
+			if vals[y] > v {
+				continue
+			}
+			x, y = find(x), find(y)
+			if x != y {
+				ans += size[x][v] * size[y][v]
+				pr[y] = x
+				size[x][v] += size[y][x]
+			}
+
+		}
+	}
+	return ans
 }
---
+++ algorithms/strings/problems.go
@@ -0,0 +1,41 @@
+package strings
+
+type Problem string
+
+const (
+	RegularExpressionMatching Problem = "pattern-matching"
+)
+
+func ProblemNames() []string {
+	return []string{
+		string(RegularExpressionMatching),
+	}
+}
+
+func IsMatch(s string, p string) bool {
+	m, n := len(s), len(p)
+	f := make([][]int, m+1)
+	for i := range f {
+		f[i] = make([]int, n+1)
+	}
+	var dfs func(i, j int) bool
+	dfs = func(i, j int) bool {
+		if j >= n {
+			return i == m
+		}
+		if f[i][j] != 0 {
+			return f[i][j] == 1
+		}
+		res := -1
+		if j+1 < n && p[j+1] == '*' {
+			if dfs(i, j+2) || (i < m && (s[i] == p[j] || p[j] == '.') && dfs(i+1, j)) {
+				res = 1
+			}
+		} else if i < m && (s[i] == p[j] || p[j] == '.') && dfs(i+1, j+1) {
+			res = 1
+		}
+		f[i][j] = res
+		return f[i][j] == 1
+	}
+	return dfs(0, 0)
+}
